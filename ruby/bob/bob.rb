49affeabc0b543cfa9156d21110dfb81ecbb5143
unicornzero <rylee.k@gmail.com>
2013-07-25 10:00:03 -0600

    initial commit

---
+++ ruby/bob/bob.rb
@@ -0,0 +1,32 @@
+class Bob
+
+  def hey(phrase = nil)
+    if is_yelling?(phrase)
+      'Woah, chill out!'
+    elsif is_question?(phrase)
+      'Sure.'  
+    elsif is_statement?(phrase)
+      'Whatever.'
+    elsif is_quiet?(phrase)
+      'Fine. Be that way!'
+    end
+  end
+
+  def is_question?(phrase)
+    phrase.match(/.+\?$/) ? true : false
+  end
+
+  def is_yelling?(phrase)
+    phrase.match(/[A-Z\W]{3,}/) ? true : false
+  end
+
+  def is_statement?(phrase)
+    phrase.match(/\w+/) ? true : false
+  end
+
+  def is_quiet?(phrase)
+    phrase.match(/\s*/) ? true : false
+  end
+
+
+end
