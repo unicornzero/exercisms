7c46cc5e8457a311ebe4bfb2e5b4a34d07e634f1
unicornzero <rylee.k@gmail.com>
2013-07-25 12:14:01 -0600

    refactored regex

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,25 +1,36 @@
 class Bob
 
   def hey(phrase = nil)
-    phrase ||= " "
-    if is_yelling?(phrase)
-      'Woah, chill out!'
-    elsif is_question?(phrase)
+    @phrase = phrase || ""
+    determine_response
+  end
+
+  def determine_response
+    @is_question = /.+\?$/
+    @is_yelling = /^[^a-z]{1,}$/
+    @is_statement = /.*[a-z]+[^?]+$/
+    @is_quiet = /^\s*$/
+ 
+    #return 'Woah, chill out!' if is_yelling?(@phrase)
+    case @phrase
+    when @is_question
       'Sure.'  
-    elsif is_statement?(phrase)
+    when @is_statement
       'Whatever.'
-    elsif is_quiet?(phrase)
+    when @is_yelling
+      'Woah, chill out!'
+    when @is_quiet
       'Fine. Be that way.'
     end
   end
 
-  def is_question?(phrase)
-    phrase.match(/.+\?$/) ? true : false
-  end
-
   def is_yelling?(phrase)
     phrase.match(/[A-Z\W]{3,}/) &&  (phrase == phrase.upcase)
+  end
 
+=begin
+  def is_question?(phrase)
+    phrase.match(/.+\?$/) ? true : false
   end
 
   def is_statement?(phrase)
@@ -29,6 +40,6 @@ class Bob
   def is_quiet?(phrase)
     phrase.match(/\s*/) ? true : false
   end
-
+=end
 
 end
