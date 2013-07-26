9ca4da2d2544e1840983fad4639d358c42088871
unicornzero <rylee.k@gmail.com>
2013-07-26 15:08:30 -0600

    merged private methods

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -2,29 +2,27 @@ class Bob
 
   def hey(phrase = "")
     phrase = phrase || ""
-    input = determine_input(phrase)
-    provide_response(input)
+    responder(phrase)
   end
 
   private
-    def determine_input(phrase)
-      if phrase.empty? 
-        :quiet
-      elsif phrase.end_with?("?")
-        :question
-      elsif phrase == phrase.upcase
-        :yelling
-      else
-        :statement
-      end
-    end
-   
-    def provide_response(input)
+    def responder(phrase)
+      input =
+        if phrase.empty? 
+          :quiet
+        elsif phrase.end_with?("?")
+          :question
+        elsif phrase == phrase.upcase
+          :yelling
+        else
+          :statement
+        end
+
       case input
-      when :question then 'Sure.'  
-      when :statement then 'Whatever.'
-      when :yelling then 'Woah, chill out!'
-      when :quiet then 'Fine. Be that way.'
+        when :question then 'Sure.'  
+        when :statement then 'Whatever.'
+        when :yelling then 'Woah, chill out!'
+        when :quiet then 'Fine. Be that way.'
       end
     end
 
