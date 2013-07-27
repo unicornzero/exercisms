f97a01133ed7cdf37cd7cc7847ce24bdf285f018
unicornzero <rylee.k@gmail.com>
2013-07-27 13:58:57 -0600

    completed DNA-RNA transcriber

--- ruby/rna-transcription/rna-transcription_test.rb
+++ ruby/rna-transcription/rna-transcription_test.rb
@@ -8,22 +8,18 @@ class DNATest < MiniTest::Unit::TestCase
   end
 
   def test_transcribes_guanosine_unchanged
-    skip
     assert_equal 'G', DNA.new("G").to_rna
   end
 
   def test_transcribes_adenosine_unchanged
-    skip
     assert_equal 'A', DNA.new("A").to_rna
   end
 
   def test_it_transcribes_thymidine_to_uracil
-    skip
     assert_equal 'U', DNA.new("T").to_rna
   end
 
   def test_it_transcribes_all_occurrences_of_thymidine_to_uracil
-    skip
     assert_equal 'ACGUGGUCUUAA', DNA.new('ACGTGGTCTTAA').to_rna
   end
 
