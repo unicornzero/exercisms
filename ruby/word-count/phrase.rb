3056cda3e6302b10196963e1b524173e875784f8
unicornzero <rylee.k@gmail.com>
2013-07-28 14:05:49 -0600

    set default hash value to 0

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -2,27 +2,19 @@ class Phrase
   attr_reader :word_count
 
   def initialize(words)
-    count(words)
-  end
-
-  private
-    def count(words)
       words_array = find_all_words(words)
       count_each_word(words_array)
     end
 
+  private
     def find_all_words(words)
       words.downcase.scan(/\w+/)
     end
 
     def count_each_word(words_array) 
-      @word_count = {}
+      @word_count = Hash.new(0)
       words_array.each do |word|
-        if @word_count.has_key?(word)
-          @word_count[word] += 1
-        else
-          @word_count[word] = 1
-        end
+      	@word_count[word] += 1
       end
     end
 end
