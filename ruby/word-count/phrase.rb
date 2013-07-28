c87d9bd88a7c7f12dbdd561db2b92622b544f672
unicornzero <rylee.k@gmail.com>
2013-07-28 11:52:17 -0600

    phrase counter is working

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -7,6 +7,22 @@ class Phrase
 
   private
     def count(words)
-      @word_count = words.downcase.scan(/\w+/)
+      words_array = find_all_words(words)
+      count_each_word(words_array)
+    end
+
+    def find_all_words(words)
+      words.downcase.scan(/\w+/)
+    end
+
+    def count_each_word(words_array) 
+      @word_count = {}
+      words_array.each do |word|
+        if @word_count.has_key?(word)
+          @word_count[word] += 1
+        else
+          @word_count[word] = 1
+        end
+      end
     end
 end
