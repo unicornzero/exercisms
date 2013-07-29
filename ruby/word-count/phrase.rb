8948c9dab1e19d1beab857daf3165c8f67220fbc
unicornzero <rylee.k@gmail.com>
2013-07-29 09:07:11 -0600

    converted from Class to Struct

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,20 +1,15 @@
-class Phrase
-  attr_reader :word_count
+Phrase = Struct.new(:phrase) do
 
-  def initialize(words)
-      words_array = find_all_words(words)
-      count_each_word(words_array)
-    end
+  def word_count
+    histogram(normalize)
+  end
 
   private
-    def find_all_words(words)
-      words.downcase.scan(/\w+/)
+    def normalize
+      phrase.downcase.scan(/\w+/)
     end
 
-    def count_each_word(words_array) 
-      @word_count = Hash.new(0)
-      words_array.each do |word|
-      	@word_count[word] += 1
-      end
+    def histogram(words) 
+      words.each_with_object(Hash.new(0)) {|w, h| h[w] += 1 }
     end
 end
