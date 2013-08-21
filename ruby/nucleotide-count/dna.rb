7624f88e2c338341f090576b1ebe5399dbf47e85
unicornzero <rylee.k@gmail.com>
2013-08-21 16:06:39 -0600

    redistributed logic to other methods

--- ruby/nucleotide-count/dna.rb
+++ ruby/nucleotide-count/dna.rb
@@ -1,22 +1,44 @@
 class DNA
-
-  def initialize(strand)
-    @strand = strand
+  def initialize(nucleic_acid)
+    @nucleic_acid = nucleic_acid
   end
 
-
-  def count(nucleotide) 
-    raise ArgumentError unless /[AGTCU]/ =~ nucleotide
-    nucleotide_counts[nucleotide] || 0
+  def count(nucleotide)
+    validate_nucleotide(nucleotide)
+    @nucleic_acid.scan(/#{nucleotide}/).length
   end
 
   def nucleotide_counts
-    histogram = {'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0}
-    @strand.scan(/[AGTC]/).each do |nucleotide| 
-      histogram[nucleotide] += 1
-    end
-    histogram
+    dna_nucleotides.each_with_object({}) {|n, h| h[n] = count(n) || 0 }
   end
 
+  private
+  
+    def validate_nucleotide(nucleotide)
+      raise ArgumentError, "Not a valid nucleotide" unless all_nucleotides.include?(nucleotide)
+    end
+
+    def dna_nucleotides
+      @dna_nucleotides ||= Nucleotides.dna
+    end
+
+    def all_nucleotides
+      @all_nucleotides ||= Nucleotides.all
+    end
 end
 
+module Nucleotides
+  ADENINE = "A"
+  THYMINE = "T"
+  GUANINE = "G"
+  CYTOCINE = "C"
+  URACIL = "U"
+
+  def self.dna
+    [ADENINE, THYMINE, CYTOCINE, GUANINE]
+  end
+
+  def self.all
+    [ADENINE, THYMINE, CYTOCINE, GUANINE, URACIL]
+  end
+end
