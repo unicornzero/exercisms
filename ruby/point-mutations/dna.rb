adf77ca48260a5eeb30d2806b6e9c6b60ae4e6de
unicornzero <rylee.k@gmail.com>
2013-12-05 22:53:41 -0700

    revision draft

--- ruby/point-mutations/dna.rb
+++ ruby/point-mutations/dna.rb
@@ -5,18 +5,18 @@ class DNA
   end
 
   def hamming_distance(strand2)
-    mutations(strand2).count
+    mutations(strand2)
   end
 
 private
   def mutations(strand2)
-    comparison_index(strand2).find_all do |i|
+    comparison_indices(strand2).count do |i|
       @strand1[i] != strand2[i]
     end
   end
 
-  def comparison_index(strand2)
-    (0..(min_strand_length(strand2) -1)).to_a
+  def comparison_indices(strand2)
+    (0..(min_strand_length(strand2) -1))
   end
 
   def min_strand_length(strand2)
