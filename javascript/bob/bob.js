24153e370334c1710e0afcf017db448fb56f34b5
Aubrey Howell and Rylee Keys <simplyaubs@gmail.com>
2016-02-15 10:48:18 -0700

    add bob exercise #codeforaubrey

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -5,10 +5,39 @@
 
 var Bob = function() {};
 
-Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+Bob.prototype = {
+  
+  shouting: function(words) {
+    if (words == words.toLowerCase()) {
+      return false
+    } else {
+      return words == words.toUpperCase();
+    };
+  },
+  
+  questioning: function(words) {
+    var last_char = words.charAt(words.length - 1);
+    return last_char == '?'
+  },
+  
+  silent: function(words) {
+    var emptiness = /^\s*$/;
+    var silence = words.match(emptiness);
+    if (silence) { return true; };
+  },
+  
+  hey: function(words) {
+    if (this.shouting(words)) {
+      return 'Whoa, chill out!';
+    };
+    if (this.questioning(words)) {
+      return 'Sure.';
+    };
+    if (this.silent(words)) {
+      return 'Fine. Be that way!';
+    };
+    return "Whatever.";
+  }
 };
 
 module.exports = Bob;
