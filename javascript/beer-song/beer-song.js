9d602a26fda9c632275a733c9d3700cdbf411509
Aubrey Howell and Rylee Keys <rylee.k@gmail.com>
2016-02-25 23:48:16 -0700

    beer-song: v3 implementation complete #codeforaubrey

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,42 +1,29 @@
 var BeerSong = function() {};
 
 BeerSong.prototype.sing = function(startVerse, endVerse) {
+  var endVerse = endVerse || 0;
   var song = '';
-  var endNum = endVerse || 0;
-  for (num = startVerse; num >= endNum; num--) {
-    song += this.verse(num);
-    num !== endNum ? song += '\n' : null;
+  for (verseNum = startVerse; verseNum >= endVerse; verseNum--) {
+    song += this.verse(verseNum);
+    verseNum !== endVerse ? song += '\n' : null;
   };
   return song;
 };
 
-BeerSong.prototype.verse = function(num) {
-  var verseText = `${_countingPhrase(num)}`;
-  if (num == 0) {
-    verseText += `Go to the store and buy some more, ${_onTheWall(_remainder(99))}.\n`;
+BeerSong.prototype.verse = function(verseNum) {
+  var verseText = `${_beerOnWall(verseNum)}, ${_pluralizeBottle(verseNum == 0 ? 'No more' : verseNum).toLowerCase()} of beer.\n`;
+  if (verseNum == 0) {
+    return verseText += `Go to the store and buy some more, ${_beerOnWall(99)}.\n`;
   } else {
-    verseText += `Take ${num === 1 ? "it" : "one"} down and pass it around, ${_onTheWall(_remainder(num - 1).toLowerCase())}.\n`;
+    return verseText += `Take ${verseNum === 1 ? "it" : "one"} down and pass it around, ${_beerOnWall(verseNum - 1).toLowerCase()}.\n`;
   }
-  return verseText;
 
-  function _countingPhrase(num) {
-    var remainder = _remainder(num);
-    return `${_onTheWall(remainder)}, ${remainder.toLowerCase()} of beer.\n`
-
+  function _beerOnWall(verseNum) { 
+    return `${_pluralizeBottle(verseNum == 0 ? 'No more' : verseNum)} of beer on the wall`;
   };
 
-  function _onTheWall(num) {
-    return `${num} of beer on the wall`
-  }
-
-  function _remainder(num) {
-    if (num == 0) {
-      return `No more bottles`;
-    } else if (num == 1) {
-      return `${num} bottle`;
-    } else {
-      return `${num} bottles`;
-    }
+  function _pluralizeBottle(qty) {
+    return qty == 1 ? `${qty} bottle` : `${qty} bottles`;
   };
 };
 
