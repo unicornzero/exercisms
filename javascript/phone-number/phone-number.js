d9a09c07f2f77a95c20d5c0571f260c528f9dda6
Aubrey Howell and Rylee Keys <rylee.k@gmail.com>
2016-02-27 23:14:14 -0700

    phone-number: v2 implementation complete #codeforaubrey

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -6,10 +6,8 @@ PhoneNumber.prototype.number = function() {
   var digits = this.rawNumber.replace(/\D+/g, '');
   if (digits.length === 11 && digits[0] == 1) {
     return digits.slice(1);
-  } else if (digits.length !== 10) {
-      return '0000000000';
   } else {
-    return digits;
+    return digits.length === 10 ? digits : '0000000000';
   }
 };
 
@@ -19,8 +17,7 @@ PhoneNumber.prototype.areaCode = function() {
 
 PhoneNumber.prototype.toString = function() {
   var num = this.number();
-  var formatted = "(" + num.slice(0, 3) + ") " + num.slice(3, 6) + "-" + num.slice(6, 10);
-  return formatted;
+  return `(${this.areaCode()}) ${num.slice(3, 6)}-${num.slice(6, 10)}`;
 };
 
 module.exports = PhoneNumber;
