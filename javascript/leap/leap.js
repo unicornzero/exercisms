720f600464fa3856e211a3663448e910dd4ceb14
Aubrey Howell and Rylee Keys <rylee.k@gmail.com>
2016-03-05 19:06:34 -0700

    leap: v2 implementation complete #codeforaubrey

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -3,21 +3,15 @@ function Year(year) {
 };
 
 Year.prototype.isLeap = function () {
-  function divisibleByFour(year) {
-    return year % 4 == 0;
-  };
-  function divisibleByOneHundred(year) {
-    return year % 100 == 0;
-  };
-  function divisibleByFourHundred(year) {
-    return year % 400 == 0;
+  function _divisibleBy(dividend, divisor) {
+    return dividend % divisor == 0;
   };
   
   var year = this.year;
-  if (divisibleByFourHundred(year)) {
+  if (_divisibleBy(year, 400)) {
     return true
   } else {
-    return divisibleByFour(year) && !divisibleByOneHundred(year);
+    return _divisibleBy(year, 4) && !_divisibleBy(year, 100);
   };
 };
 
