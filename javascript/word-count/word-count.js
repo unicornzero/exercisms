5687115bc73252ab1e11656ab8837eaa27d93b7b
Aubrey Howell and Rylee Keys <simplyaubs@gmail.com>
2016-03-05 18:02:46 -0700

    word-count: v2 implementation complete #codeforaubrey

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,10 +1,11 @@
 var Words = function() {
   this.count = function(phrase) {
-    var words = {};
-    phrase.trim().split(/\s+/).forEach((word) => {
-      words.hasOwnProperty(word) ? words[word] += 1 : words[word] = 1;
-    });
-    return words;
+    return phrase.trim().split(/\s+/).reduce((words, word) => {
+      words.hasOwnProperty(word) 
+      ? words[word] += 1 
+      : words[word] = 1;
+      return words
+    }, {});
   }
 };
 
