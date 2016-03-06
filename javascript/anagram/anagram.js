f1beccb47ddd49abd95554dfba62c3eaece8eb4d
Aubrey Howell and Rylee Keys <rylee.k@gmail.com>
2016-03-06 12:55:19 -0700

    anagram: v3 implementation complete #codeforaubrey

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,20 +1,20 @@
 var Anagram = function(baseWord) {
 
   this.matches = function(words) {
-    var words = Array.isArray(words) ? words : Array.prototype.slice.call(arguments);
+    words = Array.isArray(words) ? words : Array.prototype.slice.call(arguments);
     return words.filter((word) => { return _match(word);  });
 
     function _match(newWord) {
       return !_sameWord(newWord) && _sortLetters(baseWord) === _sortLetters(newWord);
-    };
+    }
 
     function _sortLetters(word) {
       return word.toLowerCase().split('').sort().join('');
-    };
+    }
 
     function _sameWord(newWord) {
       return baseWord.toLowerCase() === newWord.toLowerCase();
-    };
+    }
   };
 };
 
