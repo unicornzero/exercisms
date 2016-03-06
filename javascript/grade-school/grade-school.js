15e43b023d596f54750b3f45ea13b5bc4111492d
Aubrey Howell and Rylee Keys <rylee.k@gmail.com>
2016-03-05 19:49:22 -0700

    grade-school: : v1 implementation complete #codeforaubrey

---
+++ javascript/grade-school/grade-school.js
@@ -0,0 +1,22 @@
+var School = function() {
+  this._roster = {};
+
+  this.roster = function() {
+    return this._roster;
+  };
+
+  this.add = function(studentName, gradeNumber) {
+    this._roster[gradeNumber] 
+      ? this._roster[gradeNumber].push(studentName)
+      : this._roster[gradeNumber] = [studentName];
+    this._roster[gradeNumber] = this._roster[gradeNumber].sort();
+  };
+
+  this.grade = function(gradeNumber) {
+    return this._roster[gradeNumber]
+            ? this._roster[gradeNumber]
+            : [];
+  }
+};
+
+module.exports = School;
