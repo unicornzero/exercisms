ebb2f7ed184afffa2acf10a89fbd60de3bbce18c
Aubrey Howell and Rylee Keys <rylee.k@gmail.com>
2016-03-20 00:46:23 -0600

    food-chain: v3 implementation complete #codeforaubrey

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -2,7 +2,7 @@ var FoodChain = function() {
 
   this.verses = function(startVerse, endVerse) {
     var song = '';
-    for (verse = startVerse; verse <= endVerse; verse ++) {
+    for (verse = startVerse; verse <= endVerse; verse++) {
       song += this.verse(verse) + '\n';
     }
     return song;
@@ -10,6 +10,7 @@ var FoodChain = function() {
 
   this.verse = function(verse) {
     var animals = [
+      [],
       ['fly', ''],
       ['spider', 'It wriggled and jiggled and tickled inside her.\n'],
       ['bird', 'How absurd to swallow a bird!\n'],
@@ -19,22 +20,43 @@ var FoodChain = function() {
       ['cow', 'I don\'t know how she swallowed a cow!\n'],
       ['horse', 'She\'s dead, of course!\n']
     ];
-    var lyrics = `I know an old lady who swallowed a ${animals[verse - 1][0]}.\n`;
-    for (i = verse - 1; i >= 1; i--) {
-      if (i === verse - 1  || i < 2) {
-        lyrics += animals[i][1];
+  
+    var animal = (animalId) => {
+      return animals[animalId][0];
+    }
+
+    var nextAnimal = (currentAnimalId) => {
+      return animal(currentAnimalId - 1);
+    };
+
+    var reactionToConsumption = (animalId) => {
+      return animals[animalId][1];
+    };
+
+    var consumptionReasoning = (num) => {
+      return `She swallowed the ${animal(num)} to catch the ${nextAnimal(num)}.\n`
+    };
+
+    var isLastOrSecond = (iteration) => {
+      return (num === verse  || num === 2);
+    }
+
+    var lyrics = `I know an old lady who swallowed a ${animal(verse)}.\n`;
+    for (num = verse; num >= 2; num--) {
+      if (isLastOrSecond(num, verse)) {
+        lyrics += reactionToConsumption(num);
         if (verse === 8) {
           return lyrics;
         }
       }
-      lyrics += `She swallowed the ${animals[i][0]} to catch the ${animals[i - 1][0]}`;
-      lyrics += '.\n';
+      verse > 2 ? lyrics = lyrics.replace('.\nIt', ' that') : null;
+      lyrics += consumptionReasoning(num);
     }
-    if (verse > 2) {
-      lyrics = lyrics.replace('.\nIt', ' that');
-    }
-    return lyrics += "I don't know why she swallowed the fly. Perhaps she'll die.\n";
+    return verse !== 8 
+      ? lyrics += "I don't know why she swallowed the fly. Perhaps she'll die.\n" 
+      : lyrics;
   }
+
 };
 
 
