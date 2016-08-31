b2f4774620687014a103373b3acdaa47538a9117
Rylee Keysy <rylee.k@gmail.com>
2016-08-30 19:44:54 -0600

    revised ETL

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,17 +1,18 @@
-var ETL = function() {};
-
-ETL.prototype.transform = function(oldScoring) {
-  scores = Object.keys(oldScoring);
-  this.newScoring = {};
-  for (var p = 0; p < scores.length; p++) {
-    var score = scores[p];
-    var letters = oldScoring[score];
-    for (var l = 0; l < letters.length; l++) {
-      var letter = letters[l].toLowerCase();
-      this.newScoring[letter] = Number(score);
+var ETL = function() {
+  this.transform = function(oldScoring) {
+    scores = Object.keys(oldScoring);
+    this.newScoring = {};
+    for (var p = 0; p < scores.length; p++) {
+      var score = scores[p];
+      var letters = oldScoring[score];
+      for (var l = 0; l < letters.length; l++) {
+        var letter = letters[l].toLowerCase();
+        this.newScoring[letter] = Number(score);
+      }
     }
-  }
-  return this.newScoring;
+    return this.newScoring;
+  };
 };
+
 
 module.exports = ETL;
