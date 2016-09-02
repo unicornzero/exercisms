518a39bdb2d2cd70cff3bd0f3c2412e5119b4a08
Rylee Keys <rylee.k@gmail.com>
2016-09-01 22:25:29 -0600

    updated grains to use BigInt

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -1,9 +1,22 @@
+var BigInt = require('./big-integer');
+
 var Grains = function() {
-  this.square = function(num) {
-    return num == 64 ? '9223372036854775808' : String(Math.pow(2, num - 1));
+  this._square = function(squareNum) {
+    var bigI = BigInt(2)
+    return bigI.pow(squareNum - 1);
+  }
+
+  this.square = function(squareNum) {
+    return this._square(squareNum).toString();
   };
 
-  this.total = function() {return '18446744073709551615'};
+  this.total = function() {
+    var bigI = BigInt(0)
+    for (var n = 1; n <= 64; n++) {
+      bigI = bigI.add(this._square(n));
+    }
+    return bigI.toString();
+  };
 };
 
 module.exports = Grains;
