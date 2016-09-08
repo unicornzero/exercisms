277eb99f770114b36787198cfab44e716585f3de
Rylee Keys <rylee.k@gmail.com>
2016-09-08 14:21:47 -0600

    WIP- roman numerals passing

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -2,10 +2,11 @@ function toRoman(num) {
   var repeat = function(letter, quantity) {
     var string = ''
     for (var i = 1; i <= quantity; i++) {
-      string += 'I';
+      string += letter;
     }
     return string;
   }
+  
   var numerals = [
     [1000, 'M'],
     [500, 'D'],
@@ -15,78 +16,32 @@ function toRoman(num) {
     [5, 'V'],
     [1, 'I']
   ];
-  var builder = function (num) {
-    var remainder = num;
-    var string = '';
-    for (var n in numerals) {
-      var major = numerals[n];
-      var minor = numerals[Number(n+1)] || [0, ''];
-      console.log('major :', major);
-      // console.log('minor :', minor);
-      if (remainder >= Number(major[0] + minor[0])) {
-        console.log('lesser----------------------------------');
-        string += minor[1] + major[1];
-        remainder -= (minor[0] + major[0]);
-      } else if (remainder >= major[0]) {
-        console.log(remainder);
-        console.log('greater+++++++++++++++++++++++++++++++++');
-        // console.log(remainder/major[0]);
-        // console.log(major[1]);
-        // console.log(repeat(major[1], Math.floor(remainder/major[0])));
-        string += repeat(major[1], Math.floor(remainder/major[0]));
-        remainder = remainder % major[0];
-      } else {
-        console.log('else, :', Number(major[0] + minor[0]));
-      }
-      console.log('string :', string);
-    }
-    console.log('string :', string);
-    return string;
-  }
-  console.log('==================================================');
-  console.log('builder result: ', builder(num));
-
-
 
   var convertNum = function(num) {
-    var romanNumber = '';
-    if (num < 4) {
-      romanNumber += repeat('I', num);
-    } else if (num === 4) {
-      romanNumber += 'I';
-      romanNumber += 'V';
-    } else if (num === 5) {
-      romanNumber += 'V';
-    } else if (num < 9) {
-      romanNumber += 'V';
-      romanNumber += repeat('I', num - 5);
-    } else if (num === 9) {
-      romanNumber += 'I';
-      romanNumber += 'X';
-    } else if (num === 10) {
-      romanNumber += 'X';
+    var translated = '';
+    var remainder = num;
+    for (var i in numerals) {
+      var curSet = numerals[i]
+      var curNum = curSet[0];
+      var curLetter = curSet[1];
+      if (remainder >= curNum) {
+        var multiple = Math.floor(remainder / curNum);
+        translated += repeat(curLetter, remainder/curNum);
+        remainder -= curNum * multiple;
+      } else if (remainder > 0 && remainder >= curNum - 1) {
+        translated += 'I' + curLetter;
+        remainder -= (curNum + 1);
+      } else if (remainder > 10 && remainder >= curNum - 10) {
+        translated += 'X' + curLetter;
+        remainder -= (curNum - 10);
+      } else if (remainder > 100 && remainder >= curNum - 100) {
+        translated += 'C' + curLetter;
+        remainder -= (curNum - 100);
+      }
     }
-    return romanNumber;
+    return translated;
   }
-
-  if (num > 10) {
-    var romanNumber = '';
-    while (num > 10) {
-      romanNumber += convertNum(10);
-      num -= 10;
-    }
-    if (num > 0) {
-      romanNumber += convertNum(num);
-    }
-
-    console.log('romanNumber = ', romanNumber);
-    return romanNumber;
-  } else {
-    console.log('convertNum(num) = ', convertNum(num));
-    return convertNum(num);
-  }
+  return convertNum(num);
 };
-
-
 
 module.exports = toRoman;
